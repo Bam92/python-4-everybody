d50919c37e7135a2de97660bfad6a6b6befb5e34
Abel <abelmbula@protonmail.com>
2018-03-15 21:58:34 +0200

    add some conditionnal tests
    Check if the user has not type a give letter twice or more... Personalize responses according to the situation

--- guess/guess.py
+++ guess/guess.py
@@ -8,28 +8,32 @@
 # Import all necessary packages here
 
 from random import choice
-
 from functions import *
 
 print("############# - Jeu de Devinette - ###########")
 
-randomWord = pick_name()
-lettersFound = []
-theWord = ""
+randomWord = pick_name() # select a random word from the list
+lettersFound = [] # list of letters found by the user
+theWord = "" # the word being guessed by the user
+
 
 while randomWord != theWord and maxChoice > 0:
-    letter = get_letter()
+    letterInput = get_letter()
     maxChoice -=1
 
-    if letter in randomWord:
-        lettersFound.append(letter)
+
+    if letterInput in lettersFound: # if the user type a letter more than once
+        print("Vous avez deja saisi la lettre {}".format(letterInput))
+
+    elif letterInput in randomWord: # add user input in the list
+        lettersFound.append(letterInput) # if it is in the generated word
 
         theWord = get_hidden(randomWord, lettersFound)
 
-        print(theWord, maxChoice)
+        print("Bien joue, vous avancez bien. Touvez juste des lettres manquante: {}.\nNotez qu'il vous reste {} chance-s".format(theWord, maxChoice))
 
     else:
-        print("La lettre saisie n'est pas ici", maxChoice)
+        print("La lettre saisie n'est pas ici")
 
     if randomWord == theWord:
-        print("Bravo, Vous avez reussi!")
+        print("Bravo, Vous avez reussi! Le mot cache etait {}.Vous l'avez devine en {} tantative-s.".format(theWord, maxChoice))
